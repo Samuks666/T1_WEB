a9cd99967ac280d7f7696fcd7229a6c0a8e1bd78
Samuks_666 <samuelrovida@gmail.com>
2026-09-23 16:39:04 -0300

    Inicialização minima do projeto

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import process from "node:process"
 import express from "express"
+import { connectDB } from "./src/config/db.js"
+
+const port = process.env.PORT;
+
+if (!port) {
+  console.error("[application] PORT não definido em .env")
+  process.exit(1);
+}
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
+connectDB();
+
+app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
