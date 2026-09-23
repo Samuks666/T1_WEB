3e0e433ebb7ddce4d9db4583b08fcbbcc2e9a5ed
Samuks_666 <samuelrovida@gmail.com>
2026-09-23 16:34:50 -0300

    Inicialização minima do projeto

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+import process from "node:process"
+import express from "express"
 
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
